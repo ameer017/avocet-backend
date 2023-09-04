bc0d850b40f95a79fc7750cb63fc7cb7cddd1f2e
mac <mac@macs-MacBook-Air-2.local>
2023-09-04 18:52:20 +0100

    no message

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: ["https://avocet-solutions.netlify.app/"],
+  origin: ["http://localhost:5173", "https://avocet-solutions.netlify.app"],
   credentials: true, 
 }));
 
