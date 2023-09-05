553bb82011f1e487a37921cb7a14c09cbd3d076e
mac <mac@macs-MacBook-Air-2.local>
2023-09-05 10:25:48 +0100

    origin updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://avocet-frontend.vercel.app", "https://avocet-solutions.onrender.com"],
+  origin: ["http://localhost:5173", "https://avocet-frontend.vercel.app", "https://avocet-solutions.onrender.com", "smtp-mail.outlook.com", "https://console.cloud.google.com"],
   credentials: true, 
 }));
 
