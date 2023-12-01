a54e6c28f6bcebc7a16920135a7c14a787177d62
ameer017 <rajiabdullahi907@gmail.com>
2023-12-01 10:18:21 +0100

    004

--- handlers/userController.js
+++ handlers/userController.js
@@ -8,7 +8,7 @@ const Token = require("../models/tokenModel");
 const crypto = require("crypto");
 const Cryptr = require("cryptr");
 const { OAuth2Client } = require("google-auth-library");
-const { default: User } = require("../model/userModel");
+const User = require("../model/userModel");
 
 const cryptr = new Cryptr(process.env.CRYPTR_KEY);
 
@@ -33,90 +33,207 @@ const registerUser = asyncHandler(async (req, res) => {
     const userAgent = [ua.ua];
 
     const user = await User.create({
+      name,
+      email,
+      password,
+      address,
+      userAgent,
+    });
+
+    // Generate Token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      sameSite: "none",
+      secure: true,
+    });
+
+    if (user) {
+      const { _id, name, email, phone, photo, role, isVerified } = user;
+
+      res.status(201).json({
+        _id,
         name,
         email,
-        password,
+        phone,
+        photo,
         address,
-        userAgent,
+        role,
+        isVerified,
+        token,
       });
-    
-      // Generate Token
-      const token = generateToken(user._id);
-    
-      // Send HTTP-only cookie
-      res.cookie("token", token, {
-        path: "/",
-        httpOnly: true,
-        expires: new Date(Date.now() + 1000 * 86400), // 1 day
-        sameSite: "none",
-        secure: true,
-      });
-
-
-      if (user) {
-        const { _id, name, email, phone, photo, role, isVerified } = user;
-    
-        res.status(201).json({
-          _id,
-          name,
-          email,
-          phone,
-          photo,
-          address,
-          role,
-          isVerified,
-          token,
-        });
-      } else {
-        res.status(400);
-        throw new Error("Invalid user data");
-      }
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
     console.log("Error while signing up user: ", err.message);
   }
 });
 
-const registerCollector = async (req, res) => {
-    try {
-        const { name, email, password, address, role, phone} = req.body;
-        const user = await User.findOne({ $or: [{ email }, { username }] });
-  
-      if (user) {
-        return res.status(400).json({ error: "User already exists" });
-      }
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-  
-      const newUser = new User({
+const registerCollector = asyncHandler(async (req, res) => {
+  try {
+    const { name, email, password, address, role, phone } = req.body;
+
+    const userExists = await User.findOne({ $or: [{ email }] });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ error: "Collector's email already in use" });
+    }
+
+    if (password.length < 6) {
+      res.status(400);
+      throw new Error("Password must be up to 6 characters.");
+    }
+
+    if (role !== "Collector") {
+      res.status(400);
+      throw new Error("Role not specified!!");
+    }
+
+    // Get UserAgent
+    const ua = parser(req.headers["user-agent"]);
+    const userAgent = [ua.ua];
+
+    //   Create new user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      address,
+      role,
+      phone,
+      userAgent,
+    });
+
+    // Generate Token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      sameSite: "none",
+      secure: true,
+    });
+
+    if (user) {
+      const { _id, name, email, phone, photo, role, isVerified } = user;
+
+      res.status(201).json({
+        _id,
         name,
         email,
-        username,
-        password: hashedPassword,
+        phone,
+        photo,
+        address,
+        role,
+        isVerified,
+        token,
       });
-      await newUser.save();
-  
-      if (newUser) {
-        generateTokenAndSetCookie(newUser._id, res);
-  
-        res.status(201).json({
-          _id: newUser._id,
-          name: newUser.name,
-          email: newUser.email,
-          username: newUser.username,
-          bio: newUser.bio,
-          profilePic: newUser.profilePic,
-        });
-      } else {
-        res.status(400).json({ error: "Invalid user data" });
-      }
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-      console.log("Error in signupUser: ", err.message);
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
     }
-  };
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in signupUser: ", err.message);
+  }
+});
 
+const loginUser = asyncHandler(async (req, res) => {
+  try {
+    const { password, email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ error: "Invalid username or password" });
+
+
+      // Trigger 2FA for unknown UserAgent
+  const ua = parser(req.headers["user-agent"]);
+  const thisUserAgent = ua.ua;
+  console.log(thisUserAgent);
+  const allowedAgent = user.userAgent.includes(thisUserAgent);
+
+  if (!allowedAgent) {
+    // Generate 6 digit code
+    const loginCode = Math.floor(100000 + Math.random() * 900000);
+    console.log(loginCode);
+
+    // Encrypt login code before saving to DB
+    const encryptedLoginCode = cryptr.encrypt(loginCode.toString());
+
+    // Delete Token if it exists in DB
+    let userToken = await Token.findOne({ userId: user._id });
+    if (userToken) {
+      await userToken.deleteOne();
+    }
+
+    // Save Token to DB
+    await new Token({
+      userId: user._id,
+      lToken: encryptedLoginCode,
+      createdAt: Date.now(),
+      expiresAt: Date.now() + 60 * (60 * 1000), // 60mins
+    }).save();
+
+    res.status(400);
+    throw new Error("New browser or device detected");
+  }
+
+  // Generate Token
+  const token = generateToken(user._id);
+
+  if (user && isPasswordCorrect) {
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      sameSite: "none",
+      secure: true,
+    });
+
+    const { _id, name, email, phone, address, photo, role, isVerified } = user;
+
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      phone,
+      address,
+      photo,
+      role,
+      isVerified,
+      token,
+    });
+  } else {
+    res.status(500);
+    throw new Error("Something went wrong, please try again");
+  }
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in loginUser: ", error.message);
+  }
+});
 
 module.exports = {
-    registerUser
-}
+  registerUser,
+  registerCollector,
+};
