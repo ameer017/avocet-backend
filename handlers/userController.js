3addc6a8e5a2e2697bfc4547a2f0e9a760962ae7
ameer017 <rajiabdullahi907@gmail.com>
2023-12-01 10:18:11 +0100

    003

--- handlers/userController.js
+++ handlers/userController.js
@@ -76,3 +76,47 @@ const registerUser = asyncHandler(async (req, res) => {
     console.log("Error while signing up user: ", err.message);
   }
 });
+
+const registerCollector = async (req, res) => {
+    try {
+        const { name, email, password, address, role, phone} = req.body;
+        const user = await User.findOne({ $or: [{ email }, { username }] });
+  
+      if (user) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+  
+      const newUser = new User({
+        name,
+        email,
+        username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+  
+      if (newUser) {
+        generateTokenAndSetCookie(newUser._id, res);
+  
+        res.status(201).json({
+          _id: newUser._id,
+          name: newUser.name,
+          email: newUser.email,
+          username: newUser.username,
+          bio: newUser.bio,
+          profilePic: newUser.profilePic,
+        });
+      } else {
+        res.status(400).json({ error: "Invalid user data" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+      console.log("Error in signupUser: ", err.message);
+    }
+  };
+
+
+module.exports = {
+    registerUser
+}
