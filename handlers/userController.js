c612356fdfa4b459fee6169113e1533b98c3f5ef
ameer017 <rajiabdullahi907@gmail.com>
2023-12-01 12:40:36 +0100

    _012

--- handlers/userController.js
+++ handlers/userController.js
@@ -234,53 +234,53 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // Send Login Code
 const sendLoginCode = asyncHandler(async (req, res) => {
-    const { email } = req.params;
-    const user = await User.findOne({ email });
-  
-    if (!user) {
-      res.status(404);
-      throw new Error("User not found");
-    }
-  
-    // Find Login Code in DB
-    let userToken = await Token.findOne({
-      userId: user._id,
-      expiresAt: { $gt: Date.now() },
-    });
-  
-    if (!userToken) {
-      res.status(404);
-      throw new Error("Invalid or Expired token, please login again");
-    }
-  
-    const loginCode = userToken.lToken;
-    const decryptedLoginCode = encryption.decrypt(loginCode);
-  
-    // Send Login Code
-    const subject = "Login Access Code - AVOCET";
-    const send_to = email;
-    const sent_from = 'avocetsolutions@outlook.com';
-    const reply_to = "noreply@avocet.com";
-    const template = "loginCode";
-    const name = user.name;
-    const link = decryptedLoginCode;
-  
-    try {
-      await sendEmail(
-        subject,
-        send_to,
-        sent_from,
-        reply_to,
-        template,
-        name,
-        link
-      );
-      res.status(200).json({ message: `Access code sent to ${email}` });
-    } catch (error) {
-      res.status(500);
-      throw new Error("Email not sent, please try again");
-    }
+  const { email } = req.params;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // Find Login Code in DB
+  let userToken = await Token.findOne({
+    userId: user._id,
+    expiresAt: { $gt: Date.now() },
   });
+
+  if (!userToken) {
+    res.status(404);
+    throw new Error("Invalid or Expired token, please login again");
+  }
+
+  const loginCode = userToken.lToken;
+  const decryptedLoginCode = encryption.decrypt(loginCode);
+
+  // Send Login Code
+  const subject = "Login Access Code - AVOCET";
+  const send_to = email;
+  const sent_from = "avocetsolutions@outlook.com";
+  const reply_to = "noreply@avocet.com";
+  const template = "loginCode";
+  const name = user.name;
+  const link = decryptedLoginCode;
+
+  try {
+    await sendEmail(
+      subject,
+      send_to,
+      sent_from,
+      reply_to,
+      template,
+      name,
+      link
+    );
+    res.status(200).json({ message: `Access code sent to ${email}` });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, please try again");
+  }
+});
 
 // Login With Code
 const loginWithCode = asyncHandler(async (req, res) => {
@@ -384,7 +384,7 @@ const sendVerificationEmail = asyncHandler(async (req, res) => {
   // Send Email
   const subject = "Verify Your Account - AVOCET";
   const send_to = user.email;
-  const sent_from = 'avocetsolutions@outlook.com';
+  const sent_from = "avocetsolutions@outlook.com";
   const reply_to = "noreply@avocet.com";
   const template = "verifyEmail";
   const name = user.name;
@@ -438,26 +438,23 @@ const verifyUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Account Verification Successful" });
 });
 
-const logoutUser = asyncHandler(
-
-  async (req, res) => {
-    try {
-      res.cookie("token", "", {
-        path: "/",
-        httpOnly: true,
-        expires: new Date(0), // 1 day
-        sameSite: "none",
-        secure: true,
-      });
-      return res.status(200).json({ message: "Logout successful" });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-      console.log("Error in signupUser: ", err.message);
-    }
+const logoutUser = asyncHandler(async (req, res) => {
+  try {
+    res.cookie("token", "", {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(0), // 1 day
+      sameSite: "none",
+      secure: true,
+    });
+    return res.status(200).json({ message: "Logout successful" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in signupUser: ", err.message);
   }
-) 
+});
 
-const followUnFollowUser = async (req, res) => {
+const followUnFollowUser = asyncHandler(async (req, res) => {
   try {
     const { id } = req.params;
     const userToModify = await User.findById(id);
@@ -466,7 +463,7 @@ const followUnFollowUser = async (req, res) => {
     if (id === req.user._id.toString())
       return res
         .status(400)
-        .json({ error: "You cannot follow/unfollow yourself" });
+        .json({ error: "You cannot follow or un follow yourself" });
 
     if (!userToModify || !currentUser)
       return res.status(400).json({ error: "User not found" });
@@ -474,10 +471,10 @@ const followUnFollowUser = async (req, res) => {
     const isFollowing = currentUser.following.includes(id);
 
     if (isFollowing) {
-      // Unfollow user
+      // Un follow user
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
-      res.status(200).json({ message: "User unfollowed successfully" });
+      res.status(200).json({ message: "User un-followed successfully" });
     } else {
       // Follow user
       await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
@@ -488,68 +485,61 @@ const followUnFollowUser = async (req, res) => {
     res.status(500).json({ error: err.message });
     console.log("Error in followUnFollowUser: ", err.message);
   }
-};
+});
 
-const updateUser = async (req, res) => {
-  const { name, email, username, password, bio } = req.body;
-  let { profilePic } = req.body;
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
-  const userId = req.user._id;
-  try {
-    let user = await User.findById(userId);
-    if (!user) return res.status(400).json({ error: "User not found" });
+  if (user) {
+    const { _id, name, email, phone, address, photo, role, isVerified, account_Num, bank } = user;
 
-    if (req.params.id !== userId.toString())
-      return res
-        .status(400)
-        .json({ error: "You cannot update other user's profile" });
-
-    if (password) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      user.password = hashedPassword;
-    }
-
-    if (profilePic) {
-      if (user.profilePic) {
-        await cloudinary.uploader.destroy(
-          user.profilePic.split("/").pop().split(".")[0]
-        );
-      }
-
-      const uploadedResponse = await cloudinary.uploader.upload(profilePic);
-      profilePic = uploadedResponse.secure_url;
-    }
-
-    user.name = name || user.name;
-    user.email = email || user.email;
-    user.username = username || user.username;
-    user.profilePic = profilePic || user.profilePic;
-    user.bio = bio || user.bio;
-
-    user = await user.save();
-
-    // Find all posts that this user replied and update username and userProfilePic fields
-    await Post.updateMany(
-      { "replies.userId": userId },
-      {
-        $set: {
-          "replies.$[reply].username": user.username,
-          "replies.$[reply].userProfilePic": user.profilePic,
-        },
-      },
-      { arrayFilters: [{ "reply.userId": userId }] }
-    );
-
-    // password should be null in response
-    user.password = null;
-
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-    console.log("Error in updateUser: ", err.message);
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      phone,
+      address,
+      photo,
+      role,
+      isVerified,
+      account_Num, 
+      bank
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
   }
-};
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { name, email, phone, address, photo, role, isVerified } = user;
+
+    user.email = email;
+    user.name = req.body.name || name;
+    user.phone = req.body.phone || phone;
+    user.address = req.body.address || address;
+    user.photo = req.body.photo || photo;
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      phone: updatedUser.phone,
+      address: updatedUser.address,
+      photo: updatedUser.photo,
+      role: updatedUser.role,
+      isVerified: updatedUser.isVerified,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
 
 const getSuggestedUsers = async (req, res) => {
   try {
@@ -581,21 +571,314 @@ const getSuggestedUsers = async (req, res) => {
   }
 };
 
-const freezeAccount = async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
-    if (!user) {
-      return res.status(400).json({ error: "User not found" });
-    }
+// Delete User
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = User.findById(req.params.id);
 
-    user.isFrozen = true;
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  await user.deleteOne();
+  res.status(200).json({
+    message: "User deleted successfully",
+  });
+});
+
+// Get Users
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().sort("-createdAt").select("-password");
+  if (!users) {
+    res.status(500);
+    throw new Error("Something went wrong");
+  }
+  res.status(200).json(users);
+});
+
+// Get Login Status
+const loginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  
+  if (!token) {
+    return res.json(false);
+  }
+
+  // Verify token
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (verified) {
+    return res.json(true);
+  }
+  return res.json(false);
+});
+
+// upgrade user
+const upgradeUser = asyncHandler(async (req, res) => {
+  const { role, id } = req.body;
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  user.role = role;
+  await user.save();
+
+  res.status(200).json({
+    message: `User role updated to ${role}`,
+  });
+});
+
+// Send Automated emails
+const sendAutomatedEmail = asyncHandler(async (req, res) => {
+  const { subject, send_to, reply_to, template, url } = req.body;
+
+  if (!subject || !send_to || !reply_to || !template) {
+    res.status(500);
+    throw new Error("Missing email parameter");
+  }
+
+  // Get user
+  const user = await User.findOne({ email: send_to });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  const sent_from = 'avocetsolutions@outlook.com';
+  const name = user.name;
+  const link = `${process.env.FRONTEND_URL}${url}`;
+
+  try {
+    await sendEmail(
+      subject,
+      send_to,
+      sent_from,
+      reply_to,
+      template,
+      name,
+      link
+    );
+    res.status(200).json({ message: "Email Sent" });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, please try again");
+  }
+});
+
+// Forgot Password
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("No user with this email");
+  }
+
+  // Delete Token if it exists in DB
+  let token = await Token.findOne({ userId: user._id });
+  if (token) {
+    await token.deleteOne();
+  }
+
+  //   Create Verification Token and Save
+  const resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+  console.log(resetToken);
+
+  // Hash token and save
+  const hashedToken = hashToken(resetToken);
+  await new Token({
+    userId: user._id,
+    rToken: hashedToken,
+    createdAt: Date.now(),
+    expiresAt: Date.now() + 60 * (60 * 1000), // 60mins
+  }).save();
+
+  // Construct Reset URL
+  const resetUrl = `${process.env.FRONTEND_URL}/resetPassword/${resetToken}`;
+
+  // Send Email
+  const subject = "Password Reset Request - AVOCET";
+  const send_to = user.email;
+  const sent_from = 'avocetsolutions@outlook.com';
+  const reply_to = "noreply@ameer.com";
+  const template = "forgotPassword";
+  const name = user.name;
+  const link = resetUrl;
+
+  try {
+    await sendEmail(
+      subject,
+      send_to,
+      sent_from,
+      reply_to,
+      template,
+      name,
+      link
+    );
+    res.status(200).json({ message: "Password Reset Email Sent" });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, please try again");
+  }
+});
+
+// Reset Password
+const resetPassword = asyncHandler(async (req, res) => {
+  const { resetToken } = req.params;
+  const { password } = req.body;
+  console.log(resetToken);
+  console.log(password);
+
+  const hashedToken = hashToken(resetToken);
+
+  const userToken = await Token.findOne({
+    rToken: hashedToken,
+    expiresAt: { $gt: Date.now() },
+  });
+
+  if (!userToken) {
+    res.status(404);
+    throw new Error("Invalid or Expired Token");
+  }
+
+  // Find User
+  const user = await User.findOne({ _id: userToken.userId });
+
+  // Now Reset password
+  user.password = password;
+  await user.save();
+
+  res.status(200).json({ message: "Password Reset Successful, proceed to login" });
+});
+
+// Change Password
+const changePassword = asyncHandler(async (req, res) => {
+  const { oldPassword, password } = req.body;
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  if (!oldPassword || !password) {
+    res.status(400);
+    throw new Error("Please enter old and new password");
+  }
+
+  // Check if old password is correct
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  // Save new password
+  if (user && passwordIsCorrect) {
+    user.password = password;
     await user.save();
 
-    res.status(200).json({ success: true });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+    res
+      .status(200)
+      .json({ message: "Password change successful, please re-login" });
+  } else {
+    res.status(400);
+    throw new Error("Old password is incorrect");
   }
-};
+});
+
+const loginWithGoogle = asyncHandler(async (req, res) => {
+  const { userToken } = req.body;
+  //   console.log(userToken);
+
+  const ticket = await client.verifyIdToken({
+    idToken: userToken,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const payload = ticket.getPayload();
+  const { name, email, picture, sub } = payload;
+  const password = Date.now() + sub;
+
+  // Get UserAgent
+  const ua = parser(req.headers["user-agent"]);
+  const userAgent = [ua.ua];
+
+  // Check if user exists
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    //   Create new user
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+      photo: picture,
+      isVerified: true,
+      userAgent,
+    });
+
+    if (newUser) {
+      // Generate Token
+      const token = generateToken(newUser._id);
+
+      // Send HTTP-only cookie
+      res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400), // 1 day
+        sameSite: "none",
+        secure: true,
+      });
+
+      const { _id, name, email, phone, bio, photo, role, isVerified } = newUser;
+
+      res.status(201).json({
+        _id,
+        name,
+        email,
+        phone,
+        bio,
+        photo,
+        role,
+        isVerified,
+        token,
+      });
+    }
+  }
+
+  // User exists, login
+  if (user) {
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      sameSite: "none",
+      secure: true,
+    });
+
+    const { _id, name, email, phone, bio, photo, role, isVerified } = user;
+
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      phone,
+      bio,
+      photo,
+      role,
+      isVerified,
+      token,
+    });
+  }
+});
+
 
 
 module.exports = {
@@ -606,5 +889,11 @@ module.exports = {
   loginWithCode,
   sendVerificationEmail,
   verifyUser,
-  logoutUser
+  logoutUser,
+  followUnFollowUser,
+  getUser,
+  updateUser,
+  getSuggestedUsers,
+  deleteUser
 };
+
