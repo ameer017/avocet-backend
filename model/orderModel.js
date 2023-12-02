c7b5dc6032c5abb777697019aa817e5c1ddfacea
ameer017 <rajiabdullahi907@gmail.com>
2023-12-02 16:54:27 +0100

    001_day02

---
+++ model/orderModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+  {
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    weight: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+    },
+    amount: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    sellerEmail: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "received",
+    },
+    processedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    isProcessed: {
+        type: Boolean,
+        default: false
+    }
+  },
+  {
+    timestamps: true,
+    minimize: false,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
