e0a44c122d2457f07640c89248b50dd6fc96b542
ameer017 <rajiabdullahi907@gmail.com>
2023-12-05 15:12:36 +0100

    227

--- routes/plasticRoutes.js
+++ routes/plasticRoutes.js
@@ -1,27 +1,19 @@
 const express = require("express");
-const { 
-    getOrders, 
-    deleteOrder, 
-    getOrder, 
-    upgradeOrder, 
-    updateOrder, 
-    confirmOrder, 
-    createOrder,
-    sendOrderCreationEmail
-} = require("../controllers/plasticController");
+
 
 const { protect, authorOnly } = require("../middleware/authMiddleware");
+const { createOrder } = require("../handlers/orderController");
 const router = express.Router();
 
 router.post("/create", protect, createOrder);
-router.post("/confirm", protect, authorOnly, confirmOrder);
-router.get("/getOrder", protect, getOrder);
-router.get("/getOrders", protect, getOrders);
-router.patch("/updateOrder", protect, updateOrder);
+// router.post("/confirm", protect, authorOnly, confirmOrder);
+// router.get("/getOrder", protect, getOrder);
+// router.get("/getOrders", protect, getOrders);
+// router.patch("/updateOrder", protect, updateOrder);
 
-router.delete("/:id", protect, deleteOrder);
-router.post("/upgradeOrder", protect, authorOnly, upgradeOrder);
-router.post("/sendmailtocollector", protect, sendOrderCreationEmail)
+// router.delete("/:id", protect, deleteOrder);
+// router.post("/upgradeOrder", protect, authorOnly, upgradeOrder);
+// router.post("/sendmailtocollector", protect, sendOrderCreationEmail)
 
 
 module.exports = router;
