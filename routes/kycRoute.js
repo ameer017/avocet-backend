6ae3fa9a3ea1e7a02d6fac59867838aa46da0a39
ameer017 <rajiabdullahi907@gmail.com>
2024-01-02 13:39:04 +0100

    file-setup

--- routes/kycRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const createKyc = require("../controllers/kycController");
-const router = express.Router();
-
-router.post('/kyc-verification', createKyc)
-
-module.exports = router;
