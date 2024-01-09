6522b453252d79fa17b1b536fa554b8e6efa5ab9
ameer017 <rajiabdullahi907@gmail.com>
2024-01-09 10:44:35 +0100

    hello

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-// })
-// .catch((err) => console.log(err));
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
