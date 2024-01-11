ec1345ca351518f5b41519fab99106f6d2405fbb
ameer017 <rajiabdullahi907@gmail.com>
2024-01-11 11:19:47 +0100

    delete item

--- controllers/wasteController.js
+++ controllers/wasteController.js
@@ -85,6 +85,19 @@ const upgradeWaste = async (req, res) => {
 };
 
 const deleteWaste = async (req, res) => {
+  if(req.params.id * 1 > datas.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: {
+      message: null
+    },
+  });
   res.status(500).json({
     status: "error",
     message: "internal server error",
