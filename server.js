d4887115775df1dc6e9fd4af54087fd276963ee6
ameer017 <rajiabdullahi907@gmail.com>
2024-03-25 10:22:57 +0100

    backend_overhaul

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const userRoute = require("./routes/userRoutes");
-const wasteRoute = require("./routes/wasteRoute");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.use(morgan("combined"));
+const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
-app.use("/api/users", userRoute);
-app.use("/api/wastes", wasteRoute);
-
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on ${PORT}`);
-    });
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "*"],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
-  .catch((err) => console.log(err));
+);
