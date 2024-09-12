56f76b63971277c717578e479fdb1b6d6850cb29
ameer017 <rajiabdullahi907@gmail.com>
2024-09-12 22:10:00 +0100

    kingfisher up

--- routes/contactRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { reachOut, getMessages } = require("../controllers/contactHandler");
-const router = express.Router();
-
-router.post("/reachout", reachOut);
-router.get("/get-messages", getMessages);
-
-module.exports = router
--- server.js
+++ server.js
@@ -1,58 +1,31 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const errorHandler = require("./middleware/errorMiddleware");
-const wasteRoute = require("./routes/wasteRoute");
-const contactRoute = require("./routes/contactRoute");
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
-const app = express();
+const app = express()
+const PORT = 3000
 
-// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
-app.use(
-  cors({
+app.use(cors({
     origin: [
-      "http://localhost:3000",
-      "*",
-      "https://cloud.google.com/java/docs/reference/google-auth-library/latest/com.google.auth.oauth2",
-      "https://api.korapay.com/merchant/api/v1/transactions/disburse",
-      "https://earthfi.vercel.app/"
+        "http://localhost:5173",
+        "https://www.earthfi.xyz"
     ],
     credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  })
-);
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
+}))
 
-// Routes
 
 app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+    res.send("Hello Guys! Status to my welcome..")
+})
 
-app.use("/api/plastik", wasteRoute);
-app.use("/api/contacts", contactRoute);
-
-// Error Handler
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on ${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+app.listen(PORT, () => {
+    console.log(`Kingfisher hailing on line ${PORT} `)
+})
